0e9d95df5437ee65678110706410aae14b2c329e
Muhammad Azam <64076939+AazamJutt@users.noreply.github.com>
2021-10-05 19:20:52 +0500

    Added Files

---
+++ EAD_SumVariations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EAD_SumVariations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
