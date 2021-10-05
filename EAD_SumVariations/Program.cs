b68ebcff9fd52946dc96448f9512ec2f54268862
Muhammad Azam <64076939+AazamJutt@users.noreply.github.com>
2021-10-05 20:09:32 +0500

    Ádded SumVariation2

--- EAD_SumVariations/Program.cs
+++ EAD_SumVariations/Program.cs
@@ -7,6 +7,7 @@ namespace EAD_SumVariations
         static void Main(string[] args)
         {
             sum_variation1();
+            sum_variation2();
         }
 
         static void sum_variation1()
@@ -17,6 +18,17 @@ namespace EAD_SumVariations
             Console.Write("Enter Number B: ");
             int b = Int32.Parse(Console.ReadLine());
             Console.WriteLine("Sum = " + (a + b));
+        }
+
+        static void sum_variation2()
+        {
+            //Calculates Square Sum
+            Console.WriteLine("***************Sum Variation 2*************");
+            Console.Write("Enter Number A: ");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter Number B: ");
+            int b = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Square Sum = " + (a*a + b*b));
         }
     }
 }
