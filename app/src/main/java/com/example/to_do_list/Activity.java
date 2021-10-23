35a95e35e8f458f3dca16ca9cd94324f8301811d
Aazam Jutt <m.aazasultan@gmail.com>
2021-10-23 16:47:02 +0500

    Added action to acc layout onClick

---
+++ app/src/main/java/com/example/to_do_list/Activity.java
@@ -0,0 +1,4 @@
+package com.example.to_do_list;
+
+public class Activity {
+}
--- app/src/main/java/com/example/to_do_list/MainActivity.java
+++ app/src/main/java/com/example/to_do_list/MainActivity.java
@@ -3,16 +3,39 @@ package com.example.to_do_list;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     LinearLayout todoList;
-
+    Button addActivity;
+    EditText textInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         todoList = findViewById(R.id.todoList);
+        addActivity = findViewById(R.id.addActivity);
+        addActivity.setOnClickListener(this);
+        textInfo = findViewById(R.id.activityInfo);
+    }
+
+    public void addActivity(){
+        final View todoActivity = getLayoutInflater().inflate(R.layout.todo_activity,null,false);
+        TextView activity = todoActivity.findViewById(R.id.activity_info);
+        activity.setText(textInfo.getText());
+        todoList.addView(todoActivity);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.addActivity:
+                addActivity();
+        }
     }
 }
