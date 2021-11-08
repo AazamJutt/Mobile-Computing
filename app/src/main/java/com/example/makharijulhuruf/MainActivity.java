a5780deb5bb4c6c63822c32de8db52805f91b056
Aazam Jutt <m.aazasultan@gmail.com>
2021-11-08 19:47:13 +0500

    Added RTl and Setup First Screen

--- app/src/main/java/com/example/makharijulhuruf/MainActivity.java
+++ app/src/main/java/com/example/makharijulhuruf/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.makharijulhuruf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.BidiFormatter;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView letter;
+    String[] letters = {"ع","ح"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        letter = findViewById(R.id.letter);
+        letter.setText(letters[0]);
     }
 }
