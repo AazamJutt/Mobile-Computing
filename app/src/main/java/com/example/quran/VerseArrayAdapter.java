2dcd60df49154192b22d67c7dfe4643c64dd35b0
Aazam Jutt <m.aazasultan@gmail.com>
2021-11-22 15:56:30 +0500

    Added share intent on verse

--- app/src/main/java/com/example/quran/VerseArrayAdapter.java
+++ app/src/main/java/com/example/quran/VerseArrayAdapter.java
@@ -1,10 +1,12 @@
 package com.example.quran;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -31,6 +33,14 @@ public class VerseArrayAdapter extends ArrayAdapter<String> {
         View singleEntityView=inflater.inflate(R.layout.verse_view, null,true);
         TextView surahName = singleEntityView.findViewById(R.id.verse_name);
         TextView index = singleEntityView.findViewById(R.id.index);
+        Button share = singleEntityView.findViewById(R.id.shareBtn);
+        share.setOnClickListener(v->{
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, verses.get(position));
+            context.startActivity(shareIntent);
+        });
         index.setText("◀"+this.index+"."+(position+1)+"▶");
         surahName.setText(verses.get(position));
         return singleEntityView;
